fd411980ee0e7bf010ba2fe21573b42b04a470db
ChocoV <vlad-1-9-9-6@mail.ru>
2023-06-29 02:26:38 +0800

    delet import

--- src/main/java/actions/CreateConfigAction.java
+++ src/main/java/actions/CreateConfigAction.java
@@ -16,7 +16,6 @@ import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.JBTextField;
