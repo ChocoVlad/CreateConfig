cebd9b54d4f363cd4d56a3145d66c918c453ee27
Kazarin <vlad-1-9-9-6@mail.ru>
2023-06-30 02:55:46 +0800

    fix

--- src/main/java/actions/RepVersionAction.java
+++ src/main/java/actions/RepVersionAction.java
@@ -8,6 +8,9 @@ import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 
 import javax.swing.*;
 import java.awt.*;
@@ -22,28 +25,45 @@ public class RepVersionAction extends AnAction {
         Project project = e.getProject();
         if (project != null) {
             JPopupMenu popupMenu = new JPopupMenu();
-            for (String stand : new String[]{"prod", "fix", "test", "pre-test"}) {
+            for (String stand : new String[]{"prod", "fix", "fix-old", "test", "pre-test"}) {
                 JMenuItem menuItem = new JMenuItem(stand, AllIcons.Actions.Annotate);
                 menuItem.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent actionEvent) {
-                        ProgressManager.getInstance().run(new Task.Backgroundable(project, "Executing Command", true) {
+                        ProgressManager.getInstance().run(new Task.Backgroundable(project, "Checkout на ветки по стенду " + stand, true) {
                             @Override
                             public void run(@NotNull ProgressIndicator indicator) {
                                 String command = "make checkout stand=" + stand;
                                 String workingDirectory = "C:\\Users\\TensorUser\\install_environment";
                                 try {
+                                    // Стартовое информационное окно
+                                    Notification notification = new Notification("RepVersion", "Начало выполнения команды", "Выполняется команда: " + command, NotificationType.INFORMATION);
+                                    Notifications.Bus.notify(notification);
+
                                     ProcessBuilder processBuilder = new ProcessBuilder("cmd", "/c", command);
                                     processBuilder.directory(new File(workingDirectory));
                                     Process process = processBuilder.start();
                                     int exitCode = process.waitFor();
+
                                     if (exitCode == 0) {
                                         // Команда успешно выполнена
                                         System.out.println("Команда успешно выполнена.");
+
+                                        // Информационное окно при успешном выполнении
+                                        notification = new Notification("RepVersion", "Команда выполнена успешно", "Команда успешно выполнена.", NotificationType.INFORMATION);
+                                        Notifications.Bus.notify(notification);
                                     } else {
                                         // Команда завершилась с ошибкой
                                         System.out.println("Команда завершилась с ошибкой. Код возврата: " + exitCode);
+
+                                        // Информационное окно при ошибке выполнения
+                                        notification = new Notification("RepVersion", "Ошибка выполнения команды", "Выполнение команды завершилось с ошибкой. Код возврата: " + exitCode, NotificationType.ERROR);
+                                        Notifications.Bus.notify(notification);
                                     }
+
+                                    // Задержка в 4 секунды перед закрытием уведомления
+                                    Thread.sleep(4000);
+                                    notification.expire();
                                 } catch (IOException | InterruptedException e) {
                                     e.printStackTrace();
                                 }
