416a2a9583eb1693a2db068eae38ab593ff42389
ChocoV <vlad-1-9-9-6@mail.ru>
2023-07-01 15:30:26 +0800

    Сократил кода + попробовал сделать многобилдовость

--- src/main/java/actions/CreateConfigAction.java
+++ src/main/java/actions/CreateConfigAction.java
@@ -115,44 +115,7 @@ public class CreateConfigAction extends AnAction {
                                 VirtualFile destinationFile = currentFile.getParent().createChildData(this, "config.ini");
                                 destinationFile.setBinaryContent(configFile.contentsToByteArray());
 
-                                File iniFile = new File(destinationFile.getPath());
-                                iniFile.createNewFile();
-
-                                Wini ini = new Wini();
-                                ini.getConfig().setFileEncoding(StandardCharsets.UTF_8);
-                                ini.getConfig().setLowerCaseOption(false);
-
-                                ini.load(iniFile);
-
-                                if (!ini.containsKey("general")) {
-                                    ini.add("general");
-                                }
-
-                                if (!StringUtil.isEmptyOrSpaces(downloadDir)) {
-                                    ini.get("general").put("DOWNLOAD_DIR", downloadDir);
-                                }
-
-                                if (highLightActionEnabled) {
-                                    ini.get("general").put("HIGHLIGHT_ACTION", "True");
-                                }
-                                if (headlessActionEnabled) {
-                                    ini.get("general").put("HEADLESS_MODE", "True");
-                                }
-                                if (authServiceActionEnabled) {
-                                    ini.get("general").put("AUTH_SERVICE_ADDRESS", "http://dev-jenkinscontrol-service.unix.tensor.ru:8787");
-                                }
-
-                                if (testFilesActionEnabled) {
-                                    if (!ini.get("custom").containsKey("TEST_FILES")) {
-                                        VirtualFile testFilesDirectory = parentDirectory.findChild("test-files");
-                                        if (testFilesDirectory != null && testFilesDirectory.isDirectory()) {
-                                            if (!ini.containsKey("custom")) {
-                                                ini.add("custom");
-                                            }
-                                            ini.get("custom").put("TEST_FILES", testFilesDirectory.getPath());
-                                        }
-                                    }
-                                }
+                                saveConfigParameters(project);
 
                                 if (apiDataActionEnabled) {
                                     VirtualFile previewDirectory = parentDirectory.findChild("data_asserts");
@@ -164,21 +127,8 @@ public class CreateConfigAction extends AnAction {
                                         }
                                     }
                                 }
-
-                                ini.store(iniFile);
-
-                                if (destinationFile != null) {
-                                    destinationFile.refresh(false, true);
-                                }
-
-                                String configName = configFile.getName();
-                                String notificationMessage = "Установлен config: " + configName;
-                                Notification notification = new Notification("ConfigCopy", "Успешно", notificationMessage, NotificationType.INFORMATION);
-                                Notifications.Bus.notify(notification);
                             } catch (IOException ex) {
                                 ex.printStackTrace();
-                                Notification notification = new Notification("ConfigCopy", "Ошибка", "Ошибка при установке config файла", NotificationType.ERROR);
-                                Notifications.Bus.notify(notification);
                             }
                         });
                     }
