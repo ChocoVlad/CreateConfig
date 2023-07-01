b0ad0e684f80db5557f77b457c71687d3a419b7d
ChocoV <vlad-1-9-9-6@mail.ru>
2023-07-02 02:19:38 +0800

    рефактор панели настроек

--- src/main/java/actions/CreateConfigAction.java
+++ src/main/java/actions/CreateConfigAction.java
@@ -1,7 +1,3 @@
-//@formatter:off
-// -*- coding: utf-8 -*-
-//@formatter:on
-
 package actions;
 
 import com.google.gson.Gson;
@@ -21,13 +17,14 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.ini4j.Wini;
 
+
 import javax.swing.*;
-import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.event.CellEditorListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Type;
@@ -40,8 +37,6 @@ public class CreateConfigAction extends AnAction {
 
     private static final String PREF_PARAMETERS = "parameters";
     private Map<String, Parameter> parameters;
-    private DefaultTableModel tableModel;
-    private JTable table;
 
     @Override
     public void actionPerformed(AnActionEvent e) {
@@ -110,73 +105,87 @@ public class CreateConfigAction extends AnAction {
                 JPanel panel = new JPanel();
                 panel.setLayout(new BorderLayout());
 
-                // Создание модели таблицы
-                tableModel = new DefaultTableModel();
-                tableModel.addColumn("Название параметра");
-                tableModel.addColumn("Значение параметра");
-                tableModel.addColumn("Секция параметра");
-                tableModel.addColumn(" ");
+                Object[][] data = new Object[parameters.size()][5];
+                String[] columnNames = {"Active", "Название параметра", "Значение параметра", "Секция параметра", "Удалить"};
 
-                // Заполнение модели данными из параметров
-                for (String key : parameters.keySet()) {
-                    Parameter parameter = parameters.get(key);
-                    tableModel.addRow(new Object[]{key, parameter.getValue(), parameter.getSection(), null});
+                int i = 0;
+                for (Map.Entry<String, Parameter> entry : parameters.entrySet()) {
+                    data[i][0] = entry.getValue().isActive();
+                    data[i][1] = entry.getKey();
+                    data[i][2] = entry.getValue().getValue();
+                    data[i][3] = entry.getValue().getSection();
+                    data[i][4] = AllIcons.Actions.GC;
+                    i++;
                 }
 
-                // Создание таблицы и установка модели
-                table = new JTable(tableModel);
+                DefaultTableModel model = new DefaultTableModel(data, columnNames) {
+                    @Override
+                    public Class<?> getColumnClass(int columnIndex) {
+                        switch (columnIndex) {
+                            case 0:
+                                return Boolean.class;
+                            case 1:
+                            case 2:
+                            case 3:
+                            case 4:
+                                return Object.class;
+                            default:
+                                return super.getColumnClass(columnIndex);
+                        }
+                    }
+                };
 
-                // Установка ширины столбца с иконкой удаления
-                table.getColumnModel().getColumn(3).setMaxWidth(30);
+                JTable table = new JTable(model);
+                table.getColumn("Удалить").setCellRenderer(new IconRenderer());
+                table.getColumn("Удалить").setCellEditor(new IconEditor(table, model));
 
-                // Создание рендерера и редактора для столбца с иконкой удаления
-                table.getColumnModel().getColumn(3).setCellRenderer(new DeleteButtonRenderer());
-                table.getColumnModel().getColumn(3).setCellEditor(new DeleteButtonEditor());
-
-                // Добавление таблицы в панель с прокруткой
                 JScrollPane scrollPane = new JScrollPane(table);
                 panel.add(scrollPane, BorderLayout.CENTER);
 
-                // Создание панели для добавления нового параметра
-                JPanel addParameterPanel = new JPanel(new FlowLayout());
+                JPanel inputPanel = new JPanel();
+                inputPanel.setLayout(new GridLayout(1, 4, 5, 0)); // Меняем расположение на горизонтальное
 
-                JTextField paramNameField = new JTextField();
-                paramNameField.setPreferredSize(new Dimension(200, 30));
-                JTextField paramValueField = new JTextField();
-                paramValueField.setPreferredSize(new Dimension(200, 30));
-                JTextField paramSectionField = new JTextField();
-                paramSectionField.setPreferredSize(new Dimension(200, 30));
+                JTextField nameField = new JTextField();
+                inputPanel.add(labeledField("Название параметра", nameField));
 
-                addParameterPanel.add(new JLabel("Название параметра"));
-                addParameterPanel.add(paramNameField);
-                addParameterPanel.add(new JLabel("Значение параметра"));
-                addParameterPanel.add(paramValueField);
-                addParameterPanel.add(new JLabel("Секция параметра"));
-                addParameterPanel.add(paramSectionField);
+                JTextField valueField = new JTextField();
+                inputPanel.add(labeledField("Значение параметра", valueField));
 
-                JButton addParameterButton = new JButton("Добавить параметр");
-                addParameterButton.addActionListener(new ActionListener() {
+                JTextField sectionField = new JTextField();
+                inputPanel.add(labeledField("Секция параметра", sectionField));
+
+                JButton addButton = new JButton("Добавить параметр");
+                addButton.addActionListener(new ActionListener() {
                     @Override
-                    public void actionPerformed(ActionEvent e) {
-                        String name = paramNameField.getText();
-                        String value = paramValueField.getText();
-                        String section = paramSectionField.getText();
-                        if (!StringUtil.isEmptyOrSpaces(name) && !StringUtil.isEmptyOrSpaces(value) && !StringUtil.isEmptyOrSpaces(section)) {
-                            tableModel.addRow(new Object[]{name, value, section, null});
-                            parameters.put(name, new Parameter(value, section));
-                            paramNameField.setText("");
-                            paramValueField.setText("");
-                            paramSectionField.setText("");
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        String name = nameField.getText();
+                        String value = valueField.getText();
+                        String section = sectionField.getText();
+                        if (!name.isEmpty() && !value.isEmpty() && !section.isEmpty()) {
+                            model.addRow(new Object[]{Boolean.TRUE, name, value, section, AllIcons.Actions.GC});
+                            parameters.put(name, new Parameter(value, section, true));
+                            nameField.setText("");
+                            valueField.setText("");
+                            sectionField.setText("");
                         }
                     }
                 });
+                inputPanel.add(addButton);
 
-                addParameterPanel.add(addParameterButton);
+                panel.add(inputPanel, BorderLayout.SOUTH);
 
-                panel.add(addParameterPanel, BorderLayout.SOUTH);
+                int result = JOptionPane.showConfirmDialog(null, panel, "Настройки",
+                        JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
 
-                int option = JOptionPane.showOptionDialog(null, panel, "Настройки", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, null, null);
-                if (option == JOptionPane.OK_OPTION) {
+                if (result == JOptionPane.OK_OPTION) {
+                    parameters.clear();
+                    for (int j = 0; j < model.getRowCount(); j++) {
+                        Boolean active = (Boolean) model.getValueAt(j, 0);
+                        String name = (String) model.getValueAt(j, 1);
+                        String value = (String) model.getValueAt(j, 2);
+                        String section = (String) model.getValueAt(j, 3);
+                        parameters.put(name, new Parameter(value, section, active));
+                    }
                     Preferences preferences = Preferences.userNodeForPackage(getClass());
                     Gson gson = new Gson();
                     preferences.put(PREF_PARAMETERS, gson.toJson(parameters));
@@ -196,6 +205,13 @@ public class CreateConfigAction extends AnAction {
             JComponent jComponent = (JComponent) component;
             popupMenu.show(jComponent, 0, jComponent.getHeight());
         }
+    }
+
+    private Component labeledField(String label, JTextField textField) {
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(new JLabel(label), BorderLayout.NORTH);
+        panel.add(textField, BorderLayout.CENTER);
+        return panel;
     }
 
     @Override
@@ -263,11 +279,14 @@ public class CreateConfigAction extends AnAction {
                         Parameter parameter = parameters.get(key);
                         String value = parameter.getValue();
                         String section = parameter.getSection();
-                        if (!ini.containsKey(section)) {
-                            ini.add(section);
-                        }
-                        if (!StringUtil.isEmptyOrSpaces(value)) {
-                            ini.get(section).put(key, value);
+                        Boolean active = parameter.isActive();
+                        if (active) {
+                            if (!ini.containsKey(section)) {
+                                ini.add(section);
+                            }
+                            if (!StringUtil.isEmptyOrSpaces(value)) {
+                                ini.get(section).put(key, value);
+                            }
                         }
                     }
 
@@ -293,10 +312,12 @@ public class CreateConfigAction extends AnAction {
     static class Parameter {
         private final String value;
         private final String section;
+        private final Boolean active;
 
-        Parameter(String value, String section) {
+        Parameter(String value, String section, Boolean active) {
             this.value = value;
             this.section = section;
+            this.active = active;
         }
 
         public String getValue() {
@@ -306,51 +327,101 @@ public class CreateConfigAction extends AnAction {
         public String getSection() {
             return section;
         }
-    }
 
-    class DeleteButtonRenderer extends DefaultTableCellRenderer {
-        private final JButton deleteButton;
-
-        public DeleteButtonRenderer() {
-            deleteButton = new JButton(AllIcons.General.Remove);
-            deleteButton.setBorderPainted(false);
-            deleteButton.setContentAreaFilled(false);
-            deleteButton.setBackground(Color.RED); // Установка красного цвета фона кнопки
-            deleteButton.setOpaque(true); // Установка непрозрачности фона кнопки
-        }
-
-        @Override
-        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-            return deleteButton;
+        public Boolean isActive() {
+            return active;
         }
     }
 
-    class DeleteButtonEditor extends DefaultCellEditor {
-        private final JButton deleteButton;
+    class IconRenderer extends JButton implements TableCellRenderer {
 
-        public DeleteButtonEditor() {
+        public IconRenderer() {
+            setOpaque(true);
+        }
+
+        public Component getTableCellRendererComponent(JTable table, Object value,
+                                                       boolean isSelected, boolean hasFocus, int row, int column) {
+            if (isSelected) {
+                setForeground(table.getSelectionForeground());
+                setBackground(table.getSelectionBackground());
+            } else {
+                setForeground(table.getForeground());
+                setBackground(UIManager.getColor("Button.background"));
+            }
+            setIcon(AllIcons.Actions.GC);
+            setText("");
+            return this;
+        }
+    }
+
+    class IconEditor extends DefaultCellEditor {
+        protected JButton button;
+        private String label;
+        private boolean isPushed;
+        private JTable table;
+        private DefaultTableModel model;
+
+        public IconEditor(JTable table, DefaultTableModel model) {
             super(new JCheckBox());
-            deleteButton = new JButton(AllIcons.General.Remove);
-            deleteButton.setBorderPainted(false);
-            deleteButton.setContentAreaFilled(false);
-            deleteButton.addActionListener(new ActionListener() {
-                @Override
+            this.table = table;
+            this.model = model;
+            button = new JButton();
+            button.setOpaque(true);
+            button.setIcon(AllIcons.Actions.GC);
+            button.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
-                    stopCellEditing();
-                    int row = table.getSelectedRow();
-                    if (row != -1) {
-                        String paramName = (String) table.getValueAt(row, 0);
-                        parameters.remove(paramName);
-                        tableModel.removeRow(row);
-                        fireEditingStopped();
-                    }
+                    fireEditingStopped();
                 }
             });
         }
 
+        public Component getTableCellEditorComponent(JTable table, Object value,
+                                                     boolean isSelected, int row, int column) {
+            if (isSelected) {
+                button.setForeground(table.getSelectionForeground());
+                button.setBackground(table.getSelectionBackground());
+            } else {
+                button.setForeground(table.getForeground());
+                button.setBackground(table.getBackground());
+            }
+            label = "";
+            button.setText(label);
+            isPushed = true;
+            return button;
+        }
+
+        public Object getCellEditorValue() {
+            if (isPushed) {
+                int selectedRow = table.getSelectedRow();
+                if (selectedRow != -1) {
+                    String name = (String) model.getValueAt(selectedRow, 1);
+                    parameters.remove(name);
+                    model.removeRow(selectedRow);
+                }
+            }
+            isPushed = false;
+            return new String(label);
+        }
+
+        public boolean stopCellEditing() {
+            isPushed = false;
+            return super.stopCellEditing();
+        }
+
         @Override
-        public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
-            return deleteButton;
+        public boolean isCellEditable(EventObject anEvent) {
+            return true;
+        }
+
+        @Override
+        public void cancelCellEditing() {
+            isPushed = false;
+            super.cancelCellEditing();
+        }
+
+        @Override
+        public void addCellEditorListener(CellEditorListener l) {
+            super.addCellEditorListener(l);
         }
     }
 }
