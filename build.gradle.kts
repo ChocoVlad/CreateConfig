bfac035f4f8dc646be46962c8f0bd01be5285115
ChocoV <vlad-1-9-9-6@mail.ru>
2023-07-14 05:55:12 +0800

    Фиксация проекта для Pycharm

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,21 +18,20 @@ dependencies {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2022.2.4")
-    type.set("IC") // Target IDE Platform
-
+    version.set("2023.1")
+    type.set("PC") // Target IDE Platform
     plugins.set(listOf(/* Plugin Dependencies */))
 }
 
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
         options.encoding = "UTF-8"
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "17"
     }
 
     patchPluginXml {
