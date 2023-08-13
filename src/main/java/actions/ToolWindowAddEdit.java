cfe79d8137b6ac00992555b553d9714a2534fcff
ChocoV <vlad-1-9-9-6@mail.ru>
2023-08-14 02:18:50 +0700

    fix

---
+++ src/main/java/actions/ToolWindowAddEdit.java
@@ -0,0 +1,19 @@
+package actions;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.SimpleToolWindowPanel;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ToolWindowAddEdit implements ToolWindowFactory {
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        
+    }
+}
